6157b00bbca31165be25612f700294c0f960488f
bernvaughn-vm <vaughnjbernard@gmail.com>
2017-10-09 17:49:53 -0400

    Waldo now produces output for debug

--- wwaldo/wwaldo.py
+++ wwaldo/wwaldo.py
@@ -20,24 +20,33 @@ def pngToList(filename):
     for y in range(h):
         thisLine = []
         for x in range(w):
-            thisLine.append(pixels[x,y])
+            try:
+                thisLine.append(max(pixels[x,y]))
+            except TypeError:
+                thisLine.append(pixels[x,y])
         result.append(thisLine)
+        #print("This line(",len(result),": ",thisLine)
+    #print("png to list success.")
     return list(result)
 
-def printBadMap(aList):
+def printBadMap(aList,f):
+    file = open(f,"w")
     for y in aList:
+        #print("This y: ",y)
         for x in y:
-            if max(x) < 150: print("X",end="")
-            else: print("_",end="")
-        print("")
+            #print("This x: ",x)
+            if x < 150: print("X",end="",file=file)
+            else: print("_",end="",file=file)
+        print("",file=file)
+    file.close()
 
         
 def main():
     waldo = (pngToList(gResourceDir+gWaldoPNG))
-    printBadMap(waldo)
+    printBadMap(waldo,"walOut.txt")
     toSearch = (pngToList(gResourceDir+gImageToSearch))
     print("");
-    printBadMap(toSearch)
+    printBadMap(toSearch,"mapOut.txt")
 
 if __name__ == "__main__":
     main()
