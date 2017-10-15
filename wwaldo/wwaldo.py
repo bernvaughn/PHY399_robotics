c00ae5bef572c6635631809561426bc8fa450fb8
bernvaughn-vm <vaughnjbernard@gmail.com>
2017-10-15 12:53:25 -0400

    Polished program. Probably final commit. Does not find 100% of rotated waldos.

--- wwaldo/wwaldo.py
+++ wwaldo/wwaldo.py
@@ -3,14 +3,15 @@
 # Vaughn J. Bernard
 # 10/8/17
 # This program hopefully finds waldo.
-#
+# NOTE: EDIT "gImageToSearch" to change which image is searched.
 # -----------------------------------------------
 import sys
 from PIL import Image
 
 gResourceDir = "./resources/"
 gWaldoPNG = "waldo.png"
-gImageToSearch = "new_waldoSearch02.png"
+gImageToSearch = "new_waldoSearch01.png"
+gDebugMode = 0 # set to 1 to enable verbose outputs
 
 def pngToList(filename):
     '''In: filename as string that is the path and name of
@@ -75,16 +76,28 @@ def growWaldo(waldo):
         result.append(thisLine)
     return result
 
+def rotateWaldo(waldo):
+    '''In: waldo as 2-dimensional list
+    Out: 2-dimensional list that is rotated 90 degrees clockwise.
+    '''
+    result = []
+    for yi in range(len(waldo)):
+        thisLine = []
+        for xi in range(len(waldo[0])):
+            thisLine.append(waldo[len(waldo)-1-xi][yi])
+        result.append(thisLine)
+    return result
+
 def likeness(waldo,maybeWaldo):
     '''In: waldo as 2-dimensional list
     In: maybeWaldo as 2-dimensional list to compare to
-    Out: likeness as float. 1 is 100% alike, .5 is 50% alike, etc.
+    Out: likeness as float, scale of 0-1. 1 is 100% alike, .5 is 50% alike, etc.
     '''
     likeness,count = 0,0
     for yi in range(len(waldo)):
         for xi in range(len(waldo[0])):
             count+=1
-            if waldo[xi][yi] == maybeWaldo[xi][yi]: likeness += 1
+            if waldo[yi][xi] == maybeWaldo[yi][xi]: likeness += 1
     return likeness/count
 
 def find(waldo,toSearch,tolerance):
@@ -104,19 +117,17 @@ def find(waldo,toSearch,tolerance):
                 
             like = likeness(waldo,thisSearch)
             if like >=tolerance:
-                result.append([xi+round(len(waldo)/2),yi+round(len(waldo[0])/2)])
-##              #debug
-##              print("-"*79)
-##              for y in thisSearch:
-##                  print(y)
-##              print("-"*79)
-##              for y in waldo:
-##                  print(y)
-##              print("-"*79)
-##              print(xi,yi,like)
-##              print("-"*79)
-##          print("Waldo at: x =",xi,"y =",yi)
+                result.append(str(xi+round(len(waldo)/2))+" "+str(yi+round(len(waldo[0])/2)))
     return result
+
+def printBadMap(badMap):
+    '''In: badMap as 2-dimensional list
+    Prints a badMap in human-readable form. Mostly for debugging.
+    '''
+    for y in badMap:
+        for x in y:
+            print(x,end="")
+        print("")
         
 def main():
     waldo = (pngToList(gResourceDir+gWaldoPNG))
@@ -126,9 +137,49 @@ def main():
     toSearch = badList(toSearch)
     bigWaldo = growWaldo(waldo)
 
-    print("Running program...")
-    print("Waldos:",find(waldo,toSearch,0.8));
-    print("Big Waldos:",find(bigWaldo,toSearch,0.8));
+    waldo90 = rotateWaldo(waldo)
+    waldo180 = rotateWaldo(waldo90)
+    waldo270 = rotateWaldo(waldo180)
+
+    if gDebugMode == 1:
+        print("FILE TO SEARCH:",gResourceDir+gWaldoPNG)
+        print("USING MASK:",gResourceDir+gImageToSearch)
+
+        print("Running program...\n")
+
+        a = find(waldo,toSearch,0.8)
+        b = find(bigWaldo,toSearch,0.8)
+        c = find(waldo90,toSearch,0.73)
+        d = find(waldo180,toSearch,0.73)
+        e = find(waldo270,toSearch,0.73)
+
+        a.sort()
+        b.sort()
+        f = c+d+e
+        f.sort()
+        
+        print(len(a),"Waldos:\n-------")
+        for item in a:
+            print(item)
+        print("-"*7)
+        print(len(b),"Big Waldos:\n-------")
+        for item in b:
+            print(item)
+        print("-"*7)
+        print(len(f),"Rotated Waldos:\n-------")
+        for item in f:
+            print(item)
+        print("-"*7)
+    else:
+        a = find(waldo,toSearch,0.8)
+        b = find(bigWaldo,toSearch,0.8)
+        c = find(waldo90,toSearch,0.73)
+        d = find(waldo180,toSearch,0.73)
+        e = find(waldo270,toSearch,0.73)
+        waldos = sorted(a+b+c+d+e)
+        for item in waldos:
+            print(item)
+    
 
 if __name__ == "__main__":
     main()
